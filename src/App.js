a3a1110d98ade16e5358eebcfca0ad5709b9555e
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-10-12 23:28:21 +0600

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blog from "./components/Blog";
 import Error from "./components/Error";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Statistics from "./components/Statistics";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Statistics" element={<Statistics />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const activeClassName =
+    "px-3 py-1 text-indigo-500 underline hover:text-indigo-600";
+  const notActiveClassName = "px-3 py-1 hover:text-indigo-600";
+  return (
+    <div className="container mx-auto border flex justify-between mt-5">
+      <h1 className="font-bold text-3xl text-indigo-400">
+        <span className="text-white bg-indigo-400 px-2 py-1 rounded-xl">
+          Pro
+        </span>
+        <span className="pl-2">Quizzer</span>
+      </h1>
+      <div className="font-bold text-2xl text-indigo-400">
+        <NavLink
+          to="/Home"
+          className={({ isActive }) =>
+            isActive ? activeClassName : notActiveClassName
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/Statistics"
+          className={({ isActive }) =>
+            isActive ? activeClassName : notActiveClassName
+          }
+        >
+          Statistics
+        </NavLink>
+        <NavLink
+          to="/Blog"
+          className={({ isActive }) =>
+            isActive ? activeClassName : notActiveClassName
+          }
+        >
+          Blog
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
