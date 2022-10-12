f2bd330733dfd3b0472b333a2d90a1be289fd4da
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-10-12 21:20:38 +0600

    Website Title Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
