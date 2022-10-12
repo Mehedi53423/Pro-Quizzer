4ccb5bda56b77b59dd2c3c76939be5a08d91e743
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-10-12 21:49:29 +0600

    Tailwind CSS Package Installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Error from "./components/Error";
 import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+import errorNotFound from "../assets/ErrorNotFound.png";
+
+const Error = () => {
+  return (
+    <div>
+      <img src={errorNotFound} alt="404 Error Not Found" />
+    </div>
+  );
+};
+
+export default Error;
