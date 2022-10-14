66759ad28571360ef09e202aa5655b3660178d16
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-10-14 20:54:35 +0600

    Questions Added

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const Questions = ({ questions }) => {
-  return <div>Questions : {questions.length}</div>;
+const Questions = (props) => {
+  const { question, correctAnswer, options } = props.question;
+  return (
+    <div className="container mx-auto bg-slate-100 p-5 m-5">
+      <h1 className="font-bold text-xl">
+        Question :{props.number} : {question}
+      </h1>
+      <h1 className="text-lg font-semibold">
+        {options.map((option) => (
+          <h1>{option}</h1>
+        ))}
+      </h1>
+      <h1 className="text-lg font-semibold text-center">
+        Correct Answer : {correctAnswer}
+      </h1>
+    </div>
+  );
 };
 
 export default Questions;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,11 +4,23 @@ import Questions from "./Questions";
 
 const Quiz = () => {
   const data = useLoaderData();
-  const quiz = data.data.questions;
+  const quiz = data.data;
+  const { name, logo } = quiz;
+  const questions = quiz.questions;
+  let n = 1;
   return (
     <div>
-      {quiz.map((questions) => (
-        <Questions key={questions.id} quizTopic={questions}></Questions>
+      <div className="flex justify-center items-center mt-5">
+        <img src={logo} alt="Logo" className="h-40" />
+        <h1 className="font-bold text-2xl pl-5">{name}</h1>
+      </div>
+      {console.log(quiz)}
+      {questions.map((question) => (
+        <Questions
+          key={question.id}
+          question={question}
+          number={n++}
+        ></Questions>
       ))}
     </div>
   );
