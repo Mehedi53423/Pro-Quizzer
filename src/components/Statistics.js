364a1efce51cb137d6ddf148e11973aa50e76e65
Md. Mehedi Hasan <mehedi53423@gmail.com>
2022-10-17 22:33:40 +0600

    Responsive Statistics Page Added

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -43,10 +43,24 @@ const Statistics = () => {
       <h1 className="text-center font-bold text-2xl mb-5 text-indigo-400">
         Statistics
       </h1>
-      <div className="flex justify-center">
+      <div className="md:flex justify-center hidden">
         <LineChart
           width={600}
           height={300}
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </div>
+      <div className="flex justify-center md:hidden">
+        <LineChart
+          width={300}
+          height={150}
           data={data}
           margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
         >
